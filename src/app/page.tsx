4469a89b0c665d71c0ccba9aba539191ad6f7db9
Noé Júnior <153394025+noejunior792@users.noreply.github.com>
2024-11-14 19:16:22 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className='space-y-2 mt-10'>
       <h1 className='text-3xl font-bold sm:text-5xl '>Share Your Data</h1>
       <p className='max-w-[600px] text-muted-foreground md:text-xl'>
-        Our ap makes essay to share your JSON data with others. Simply
+        Our app makes essay to share your JSON data with others. Simply
         authenticate and upload your data
       </p>
     </div>
